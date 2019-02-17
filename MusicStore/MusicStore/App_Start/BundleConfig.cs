33002da8effc6e067e92a0e08741e426fcb25b11
Ricardo Mauro <ricardo.demauro@gmail.com>
2019-02-17 15:59:39 -0300

    fixing shopping cart page.

--- MusicStore/MusicStore/App_Start/BundleConfig.cs
+++ MusicStore/MusicStore/App_Start/BundleConfig.cs
@@ -19,7 +19,6 @@ namespace MusicStore
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/popper.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
---
+++ MusicStore/MusicStore/Infrastructure/HtmlHelpers/AlbumHtmlHelper.cs
@@ -0,0 +1,30 @@
+﻿using MusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+
+namespace MusicStore.Infrastructure.HtmlHelpers
+{
+    public static class AlbumHtmlHelper
+    {
+        public static MvcHtmlString AlbumLink(this HtmlHelper helper, Album album, string action, string controller)
+        {
+            UrlHelper urlHelper = new UrlHelper(helper.ViewContext.RequestContext);
+
+            string link = urlHelper.Action(action, controller, new { id = album.AlbumId });
+
+            string value = $"<a href=\"{link}\">" +
+                   $"<img alt=\"{album.Title}\" src=\"{album.AlbumArtUrl}\" />" +
+                      $"<span>{album.Title}</span>" +
+                  "</a>";
+
+            return new MvcHtmlString(value);
+        }
+    }
+}
